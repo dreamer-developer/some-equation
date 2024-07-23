fb777371b9661c9a45b417a8c6b8e7dc8db057ad
Polina <dasyuk.polina@gmail.com>
2024-07-23 19:21:54 +0300

    Update Test1.cpp

--- Test1.cpp
+++ Test1.cpp
@@ -11,7 +11,7 @@ int main() {
         double eps = 0.001;
         Equation equation;
 
-        if (std::isnan(equation.solveEquation(a,b,c,eps))){
+        if (!std::isnan(equation.solveEquation(a,b,c,eps))){
             cout << "x^2-1 = 0 не имеет корней. Тест пройден." << std::endl;
         //assert(!std::isnan(equation.solveEquation(a,b,c,eps)));
         } 
